cfa6c973f4ecd0707fe66369e892a1e4046e9f3a
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-12-02 17:42:25 +0100

    wiseconnect: Remove dependency on sli_cmsis_os2_ext_task_register.h
    Wiseconnect relies on an extension of CMSIS API to store the status of
    the firmware. This API does not exist on Zephyr. Fortunately, the
    firmware status is never read. So we can drop this code without breaking
    anything.
    
    Upstream-status: Pending
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- wiseconnect/components/protocol/wifi/inc/sl_wifi_callback_framework.h
+++ wiseconnect/components/protocol/wifi/inc/sl_wifi_callback_framework.h
@@ -31,10 +31,6 @@
 
 #include "sl_wifi_device.h" // To access the device specific structs
 #include <stdint.h>
-#include "sli_cmsis_os2_ext_task_register.h"
-
-/// External variable representing the index of the thread local array at which the firmware status will be stored.
-extern sli_task_register_id_t sli_fw_status_storage_index;
 
 /** \addtogroup WIFI_CALLBACK_FRAMEWORK Callback Framework
   * \ingroup SL_WIFI
@@ -519,21 +515,4 @@ static inline sl_status_t sl_wifi_set_transceiver_callback(sl_wifi_transceiver_c
   return sl_wifi_set_callback(SL_WIFI_TRANSCEIVER_EVENTS, (sl_wifi_callback_function_t)function, optional_arg);
 }
 
-/***************************************************************************/ /**
- * @brief 
- *   Retrieves the saved thread-specific firmware status value.
- *  
- * @details
- *   This function fetches the firmware status value that is specific to the current thread.
- * 
- * @return
- *   sl_status_t. See [Status Codes](https://docs.silabs.com/gecko-platform/latest/platform-common/status) and [WiSeConnect Status Codes](../wiseconnect-api-reference-guide-err-codes/wiseconnect-status-codes) for details.
- ******************************************************************************/
-static inline sl_status_t sl_wifi_get_saved_firmware_status(void)
-{
-  sl_status_t status = SL_STATUS_FAIL;
-
-  sli_osTaskRegisterGetValue(NULL, sli_fw_status_storage_index, &status);
-  return status;
-}
 /** @} */
