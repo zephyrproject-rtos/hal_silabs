e106f512323421d1487a74883e31212e5a2b5721
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2025-01-10 12:12:07 +0100

    simplicity_sdk: Update import script
    Prepare for import of SiSDK 2024.12.0
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- scripts/patch_simplicity_sdk.sh
+++ scripts/patch_simplicity_sdk.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# Copyright (c) 2025 Silicon Laboratories Inc.
+# SPDX-License-Identifier: Apache-2.0
 
 # Add missing SecureFault interrupt number to device headers
 sed -i '' "s/\(UsageFault_IRQn.*\)/\1\n#if defined(CONFIG_ARM_SECURE_FIRMWARE)\n  SecureFault_IRQn       = -9,\n#endif/" simplicity_sdk/platform/Device/SiliconLabs/*/Include/*.h
+
+# Rename CONCAT macros conflicting with Zephyr macros
+sed -i '' "s/ _CONCAT_/ _SL_CONCAT_/" simplicity_sdk/platform/common/inc/sl_common.h
+sed -i '' "s/   first/first/" simplicity_sdk/platform/common/inc/sl_common.h
+
+# Replace legacy Kconfig option name
+sed -i '' "s/CONFIG_SOC_FAMILY_EXX32/__ZEPHYR__/" simplicity_sdk/platform/emlib/inc/em_ramfunc.h
