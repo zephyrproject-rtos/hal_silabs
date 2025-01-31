5eea6c13b836e7ad6839326089aa7030c24511a9
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2025-01-31 11:10:59 +0100

    scripts: Import platform/security from Simplicity SDK
    Update import_simplicity_sdk.py to add platform/security
    content.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- scripts/patch_simplicity_sdk.sh
+++ scripts/patch_simplicity_sdk.sh
@@ -14,3 +14,6 @@ sed -i '' "s/CONFIG_SOC_FAMILY_EXX32/__ZEPHYR__/" simplicity_sdk/platform/emlib/
 
 # Rename MAX macro conflicting with Zephyr macro
 sed -i '' "s/MAX(/_SL_MAX(/" simplicity_sdk/platform/radio/rail_lib/plugin/pa-conversions/pa_conversions_efr32.c
+
+# Add Zephyr OS abstraction for crypto
+sed -i '' "s/#\(if defined(SL_CATALOG_MICRIUMOS_KERNEL_PRESENT)\)/#if defined(__ZEPHYR__) \&\& defined(SL_SE_MANAGER_THREADING)\n  #include \"sli_psec_osal_zephyr.h\"\n  #define SLI_PSEC_THREADING\n#el\1/" simplicity_sdk/platform/security/sl_component/sli_psec_osal/inc/sli_psec_osal.h
