zephyrproject-rtos/hal_silabs

commit 95e957408ddd967ac4b69dc32096bd3793abb76c

simplicity_sdk: Update autogen python script
Adding xg28 to devices to generate xg28-clock.h. Import pg23 and pg28/fg28 platform files.

Signed-off-by: Christoph Jans <jans.christoph@gmail.com>

scripts/import_simplicity_sdk.py
@@ -23,12 +23,14 @@ paths = [
   "platform/common/src/sl_slist.c",
   "platform/Device/SiliconLabs/EFR32BG2[279]/Include/*.h",
   "platform/Device/SiliconLabs/EFR32BG2[279]/Source/system_*.c",
-  "platform/Device/SiliconLabs/EFR32FG2[3]/Include/*.h",
-  "platform/Device/SiliconLabs/EFR32FG2[3]/Source/system_*.c",
+  "platform/Device/SiliconLabs/EFR32FG2[38]/Include/*.h",
+  "platform/Device/SiliconLabs/EFR32FG2[38]/Source/system_*.c",
   "platform/Device/SiliconLabs/EFR32MG2[149]/Include/*.h",
   "platform/Device/SiliconLabs/EFR32MG2[149]/Source/system_*.c",
   "platform/Device/SiliconLabs/EFR32ZG2[3]/Include/*.h",
   "platform/Device/SiliconLabs/EFR32ZG2[3]/Source/system_*.c",
+  "platform/Device/SiliconLabs/EFM32PG2[38]/Include/*.h",
+  "platform/Device/SiliconLabs/EFM32PG2[38]/Source/system_*.c",
   "platform/Device/SiliconLabs/MGM24/Include/*.h",
   "platform/Device/SiliconLabs/MGM24/Source/system_*.c",
   "platform/driver/gpio/inc/*.h",

scripts/gen_clock_control.py
@@ -29,6 +29,10 @@ devices = {
 		"bits": "platform/Device/SiliconLabs/EFR32BG27/Include/efr32bg27_cmu.h",
 		"nodes": "platform/service/device_manager/clocks/sl_device_clock_efr32xg27.c"
   },
+	"xg28": {
+		"bits": "platform/Device/SiliconLabs/EFR32FG28/Include/efr32fg28_cmu.h",
+		"nodes": "platform/service/device_manager/clocks/sl_device_clock_efr32xg28.c"
+  },
 	"xg29": {
 		"bits": "platform/Device/SiliconLabs/EFR32BG29/Include/efr32bg29_cmu.h",
 		"nodes": "platform/service/device_manager/clocks/sl_device_clock_efr32xg29.c"

commit d3f32394df54896bb2d50109f8144aecd6cdc02f

simplicity_sdk: Align import script with content
Commit 95e95740 ("simplicity_sdk: Update autogen python script") changes
import_simplicity_sdk.py to imports files for PG23, PG28 and FG28.
However it seems the support for FG28 was unintentional since the
respective files have not been imported in the tree.

Fixes: 95e95740 ("simplicity_sdk: Update autogen python script")
Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

scripts/import_simplicity_sdk.py
@@ -23,8 +23,8 @@ paths = [
   "platform/common/src/sl_slist.c",
   "platform/Device/SiliconLabs/EFR32BG2[279]/Include/*.h",
   "platform/Device/SiliconLabs/EFR32BG2[279]/Source/system_*.c",
-  "platform/Device/SiliconLabs/EFR32FG2[38]/Include/*.h",
-  "platform/Device/SiliconLabs/EFR32FG2[38]/Source/system_*.c",
+  "platform/Device/SiliconLabs/EFR32FG2[3]/Include/*.h",
+  "platform/Device/SiliconLabs/EFR32FG2[3]/Source/system_*.c",
   "platform/Device/SiliconLabs/EFR32MG2[149]/Include/*.h",
   "platform/Device/SiliconLabs/EFR32MG2[149]/Source/system_*.c",
   "platform/Device/SiliconLabs/EFR32ZG2[3]/Include/*.h",